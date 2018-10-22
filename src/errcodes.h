fc4b700476ca062c583efa8df05acec14436a4d6
alysson <alysson.cirilo@gmail.com>
2018-10-22 20:33:52 -0300

    fix indentation in errcodes.h

--- src/errcodes.h
+++ src/errcodes.h
@@ -5,13 +5,13 @@
  * @brief Possible error codes in the project.
  */
 enum errcodes {
-	E_SUCCESS, 			/**< Success value */
-	E_GETADDRINFO, 		/**< Error code if getaddrinfo() fails */
-	E_BIND, 			/**< Error code if it was not possible to bind() in the specified port */
-	E_LISTEN, 			/**< Error code if liste() fails */
-	E_BAD_ARGS, 		/**< Error code if the user gave a bad input */
-	E_CONNECT, 			/**< Error code if connect() fails */
-	E_PTHREAD_CREATE 	/**< Error code if it was not possible to create a new thread */
+	E_SUCCESS,          /**< Success value */
+	E_GETADDRINFO,      /**< Error code if getaddrinfo() fails */
+	E_BIND,             /**< Error code if it was not possible to bind() in the specified port */
+	E_LISTEN,           /**< Error code if liste() fails */
+	E_BAD_ARGS,         /**< Error code if the user gave a bad input */
+	E_CONNECT,          /**< Error code if connect() fails */
+	E_PTHREAD_CREATE    /**< Error code if it was not possible to create a new thread */
 };
 
 #endif

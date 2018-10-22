41416ef4d97084690076878aaa4ea8ee11be625f
alysson <alysson.cirilo@gmail.com>
2018-10-22 20:31:15 -0300

    initial documentation on errcodes.h

--- src/errcodes.h
+++ src/errcodes.h
@@ -1,14 +1,17 @@
 #ifndef ERRCODES_H
 #define ERRCODES_H
 
+/**
+ * @brief Possible error codes in the project.
+ */
 enum errcodes {
-	E_SUCCESS,
-	E_GETADDRINFO,
-	E_BIND,
-	E_LISTEN,
-	E_BAD_ARGS,
-	E_CONNECT,
-	E_PTHREAD_CREATE
+	E_SUCCESS, 			/**< Success value */
+	E_GETADDRINFO, 		/**< Error code if getaddrinfo() fails */
+	E_BIND, 			/**< Error code if it was not possible to bind() in the specified port */
+	E_LISTEN, 			/**< Error code if liste() fails */
+	E_BAD_ARGS, 		/**< Error code if the user gave a bad input */
+	E_CONNECT, 			/**< Error code if connect() fails */
+	E_PTHREAD_CREATE 	/**< Error code if it was not possible to create a new thread */
 };
 
 #endif
